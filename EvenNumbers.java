0407d602d048a9d9ff2bc2e5f80e18a54ec3d5dd
Harini-7 <135444728+Harini-7@users.noreply.github.com>
2023-06-03 11:39:06 -0500

    Create EvenNumbers.java
    even

---
+++ EvenNumbers.java
@@ -0,0 +1,11 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int n = 20; // Change this value to adjust the range of even numbers
+
+        System.out.println("Even numbers from 1 to " + n + ":");
+
+        for (int i = 2; i <= n; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
